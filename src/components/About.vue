c5f0033cc3c5f7a2a4eba4c2cff6b82fe84f9cd1
Denis Kutsenko <deniskutchenko008@gmail.com>
2019-05-23 23:01:01 +0200

    update About app

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    About page
+  	<h1 style="text-align:center;font-family:calibri;margin-top:0.2em">About Sneep <font style="color:#1e9498;">it!</font></h1>
+    <p style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;text-indent: 1.5em;">Sneep it - this is a completely new platform with a huge base of ready-made templates for letters, documents, forms, code etc.Thanks to our platform, you will never again have questions like "What should look like an official letter", "How to politely refuse my boss" and much more... Just take and copy the template you need, now it is not a problem anymore! Sneep it provides our users with the following functions:</p>
+    <ul style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;">
+    <li>Authentication and registration of users,</li>
+    <li>Adding, creating and editing mock-ups,</li>
+    <li>Adding your favorite snippets to your favorites,</li>
+    <li>Search,</li>
+    <li>Change the language,</li>
+    </ul>
+    <p style="text-align:center;">
+    <img src="http://www.alcdsb.on.ca/PublishingImages/Lists/Announcements/EditForm/letter.png"></p>
+    <p style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;">The functionality of the application is constantly updated. We advise to follow the updates.</p>
   </div>
 </template>
