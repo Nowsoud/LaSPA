958fb463f2587a2a7f2ce3f9edf521f293a9061f
Denis Kutsenko <deniskutchenko008@gmail.com>
2019-05-23 23:29:37 +0200

    cosmetic refactoring of About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
   	<h1 style="text-align:center;font-family:calibri;margin-top:0.2em">About Sneep <font style="color:#1e9498;">it!</font></h1>
-    <p style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;text-indent: 1.5em;">Sneep it - this is a completely new platform with a huge base of ready-made templates for letters, documents, forms, code etc.Thanks to our platform, you will never again have questions like "What should look like an official letter", "How to politely refuse my boss" and much more... Just take and copy the template you need, now it is not a problem anymore! Sneep it provides our users with the following functions:</p>
+    <p style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;text-indent: 1.5em;">
+    	Sneep it - this is a completely new platform with a huge base 	of ready-made templates for letters, documents, forms, code etc. Thanks to our platform, you will never again have questions like "What should look like an official letter", "How to politely refuse my boss" and much more... Just take and copy the template you need, now it is not a problem anymore! Sneep it provides our users with the following functions:
+    </p>
     <ul style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;">
     	<li>Authentication and registration of users,</li>
     	<li>Adding, creating and editing mock-ups,</li>
@@ -10,9 +12,10 @@
     	<li>Change the language,</li>
     </ul>
     <p style="text-align:center;">
-    <img src="http://www.alcdsb.on.ca/PublishingImages/Lists/Announcements/EditForm/letter.png"></p>
+    	<img src="http://www.alcdsb.on.ca/PublishingImages/Lists/Announcements/EditForm/letter.png">
+    </p>
     <blockquote>
-    	<p style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;text-indent: 1.5em;">The functionality of the application is constantly updated. We advise to follow the updates.We will be grateful for any feedback about our application. Because you users are our motivation to continually improve our product. Thank you very much for being with us!</p>
+    	<p style="margin-left:2%;margin-top:1%;margin-right:2%;font-size:1.3em;text-indent: 1.5em;">The functionality of the application is constantly updated. 	We advise to follow the updates.We will be grateful for any feedback about our application. Because you users are our motivation to continually improve our product. Thank you very much for being with us!</p>
     	<footer style="margin-left:4%;margin-top:1%;margin-right:2%;font-size:1em;"><i> - Development team</i></footer>
     </blockquote>
     <hr style="margin-left:2%;margin-right:2%;">
